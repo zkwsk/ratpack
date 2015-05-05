84993e8fac33d1b3040c34765382c376a135c76c
Troels Busk-Jepsen <troelsbusk@gmail.com>
2015-05-06 01:44:36 +0200

    Band images on wall

--- app/js/wall.js
+++ app/js/wall.js
@@ -2,20 +2,22 @@
 /* global freewall*/
 
 
-var colour = [
-  // "bg-accent",
-  // "bg-primary",
-  "bg-primary-dark",
-  // "bg-black"
-];
-var index = 1;
-$(".free-wall .item").each(function() {
-  var colorSet = colour[colour.length * Math.random() << 0];
-  $(this).addClass(colorSet)//.append($('<h3>' + index++ + '</h3>'));
-});
+// var colour = [
+//   "bg-accent",
+//   "bg-primary",
+//   "bg-primary-dark",
+//   "bg-black"
+// ];
+// var index = 1;
+// $(".free-wall .item").each(function() {
+//   var colorSet = colour[colour.length * Math.random() << 0];
+//   $(this).addClass(colorSet)//.append($('<h3>' + index++ + '</h3>'));
+// });
 
 $(function() {
-  var wall = new freewall("#freewall");
+  var wall = new freewall('#freewall');
+  var filename;
+
   wall.reset({
     selector: '.level1',
     cellW: 320,
@@ -28,6 +30,15 @@ $(function() {
     }
   });
   wall.fitZone();
-  $(window).trigger("resize");
+  $(window).trigger('resize');
+
+  // var $this;
+  // $('i').each(function(){
+  //   $this = $(this);
+  //   filename = ($this.attr('class') + '.jpg').replace('tile-','');
+  //   $this.wrap('<a href="/images/tiles/large/' + filename + '"></a>');
+  // });
 });
+
+
 
