0d5f5c1a8670c949fef22c215cadb5ae87209a7f
Troels Busk-Jepsen <troelsbusk@gmail.com>
2015-05-09 19:03:47 +0200

    color picker

--- app/js/colors.js
+++ app/js/colors.js
@@ -1,5 +1,8 @@
-$(function(){
+'use strict';
 
-  
-
+$('.theme-picker a').click(function(){
+  var $this = $(this);
+  var color = $this.data('color');
+  $('body').removeClass().addClass('alternate-theme-' + color);
+  return false;
 });
