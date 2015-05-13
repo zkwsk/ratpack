f5c6dfe51aa933dda9444e997d8f6eaf5286e942
Troels Busk-Jepsen <troelsbusk@gmail.com>
2015-05-13 19:28:02 +0200

    Prerelease

--- app/js/colors.js
+++ app/js/colors.js
@@ -1,8 +1,8 @@
-'use strict';
+// 'use strict';
 
-$('.theme-picker a').click(function(){
-  var $this = $(this);
-  var color = $this.data('color');
-  $('body').removeClass().addClass('alternate-theme-' + color);
-  return false;
-});
+// $('.theme-picker a').click(function(){
+//   var $this = $(this);
+//   var color = $this.data('color');
+//   $('body').removeClass().addClass('alternate-theme-' + color);
+//   return false;
+// });
